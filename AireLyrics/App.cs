3e5e1f8184e340f2ac0cfbb8f263a4f2da3f16f4
Mark Schofield <mschofield73@gmail.com>
2022-08-06 04:54:44 +0100

    Create intial entry with artist argument

---
+++ AireLyrics/App.cs
@@ -0,0 +1,8 @@
+﻿// Set up host and register services
+public class App
+{
+    public async Task Run(string artist)
+    {
+        
+    }
+}
--- AireLyrics/Program.cs
+++ AireLyrics/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Set up host and register services
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+
+await BuildCommandLine()
+    .UseHost(_ => Host.CreateDefaultBuilder(), host =>
+    {
+        host.ConfigureServices(services =>
+        {
+            services.AddSingleton<App>();
+        });
+    })
+    .UseDefaults()
+    .Build()
+    .InvokeAsync(args);
+
+// Here we build our command line with a required artist option
+static CommandLineBuilder BuildCommandLine()
+{
+    var root = new RootCommand("Search for an artist.")
+    {
+        new Option<string>("--artist")
+        {
+            Name = "Artist",
+            Description = "The name of the artist to search for."
+        }
+    };
+
+    root.Handler = CommandHandler.Create<string, IHost>(Run);
+    return new CommandLineBuilder(root);
+}
+
+static async Task Run(string artist, IHost host)
+{
+    // Ensure that the user has entered a valid string
+    while (string.IsNullOrWhiteSpace(artist))
+    {
+        Console.WriteLine("Please specify an artist to search for.");
+        artist = Console.ReadLine();
+    }
+
+    var app = host.Services.GetRequiredService<App>();
+    await app.Run(artist);
+}
